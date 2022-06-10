e13e2f842071d1dee547328c208c851235b68e6c
Кочеткова Екатерина <94628140+EkaKochetkova@users.noreply.github.com>
2022-06-10 21:38:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,90 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int prior(char operation) {
+    switch (operation) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '-':
+        return 2;
+    case '+': 
+        return 2;
+    case '*': 
+        return 3;
+    case '/': 
+        return 3;
+    case ' ': 
+        return 4;
+    default: 
+        return 5; 
+    }
 }
 
+int calc(char operation, int a, int b) {
+    switch (operation) {
+    case '+': 
+        return a + b;
+    case '-': 
+        return a - b;
+    case '*': 
+        return a * b;
+    case '/': 
+        return a / b;
+    default: 
+        return 0;
+    }
+}
+
+std::string infx2pstfx(std::string inf) {
+std::string result;
+    TStack <char, 100> stack;
+    for (int i = 0; i < inf.length(); i++) {
+      if (prior(inf[i]) == 5) {
+        result.push_back(inf[i]);
+        result.push_back(' ');
+      } else {
+        if (prior(inf[i]) == 4) {
+          continue;
+        } else if (prior(inf[i]) > prior(stack.get()) || prior(inf[i]) == 0) {
+          stack.push(inf[i]);
+        }
+        else if (stack.isEmpty()) {
+          stack.push(inf[i]);
+        }
+        else if (prior(inf[i]) == 1) {
+          while (prior(stack.get()) != 0) {
+            result.push_back(stack.get());
+            result.push_back(' ');
+            stack.pop();
+          }
+          stack.pop();
+        } else {
+          while (!stack.isEmpty() && (prior(inf[i]) <= prior(stack.get()))) {
+            result.push_back(stack.get());
+            result.push_back(' ');
+            stack.pop();
+          }
+          stack.push(inf[i]);
+        }
+      }
+    }
+
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  TStack <int, 100> stack;
+  int result = 0;
+  for (int i = 0; i < pref.length(); i++) {
+    if (prior(pref[i]) == 4) {
+      stack1.push(pref[i] - '0');
+    }
+    else if (prior(pref[i]) < 4) {
+      int a = stack.get();
+      int b = stack.get();
+      stack1.pop();
+      stack1.push(calcul(pref[i], a, b));
+    }
+  }
+  result = stack1.get();
+  return result;
 }
